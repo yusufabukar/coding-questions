149b91f87ea3f74157456d445ea12bc410648704
Yusuf <65050809+yusufabukar@users.noreply.github.com>
2020-11-24 14:19:34 +0000

    Add files via upload
    Create a function that reverses a string.

---
+++ reverseAString.js
@@ -0,0 +1,35 @@
+// Create a function that reverses a string.
+
+// My Solution
+function reverseString(string) {
+	let stringArray = string.split('');
+	let reverseArray = [];
+
+	for (let i = stringArray.length - 1; i >= 0; i--) {
+		reverseArray.push(stringArray[i]);
+	};
+
+	return reverseArray.join('');
+};
+
+// Other Solutions
+
+function reverseString2(string) {
+	if (!string || typeof string != 'string' || string.length < 2) {
+		return string;
+	};
+
+	const reverseArray = [];
+
+	for (let i = string.length - 1; i >= 0; i--) {
+		reverseArray.push(string[i]) // You can access string indices like arrays
+	}
+
+	return reverseArray.join('');
+};
+
+function reverse3(string) {
+	return string.split('').reverse().join(''); // Array.reverse() method removes need for for loop
+};
+
+const reverse4 = string => [...string].reverse.join(''); // [...string] spread operator
